4b193b04d13476e2e31142d59a10024a196fef46
Xavier <xachuen@gmail.com>
2024-07-24 14:09:34 -0700

    Add clerk log in customization button

--- front_end/components/Pages/StartPage/StartPage.jsx
+++ front_end/components/Pages/StartPage/StartPage.jsx
@@ -4,6 +4,8 @@ import styles from './StartPage.module.css';
 import Logo from '../../Logo/Logo';
 import GetStarted from './subcomponents/GetStarted/GetStarted';
 
+import { SignedOut, SignedIn, SignOutButton, SignInButton } from '@clerk/clerk-react';
+
 const StartPage = () => {
   return ( 
     <div className={styles.StartPage}>
@@ -11,7 +13,16 @@ const StartPage = () => {
       <p className={styles.StartPageParagraph}>Welcome the CalenPlan, to calendar app to plan with you friends!
         Get started by signing in and invite friends to start planning!
       </p>
-      <GetStarted/>
+      <SignedOut>
+        <SignInButton>
+          <button className={`${styles.GetStarted}`}>Sign In</button>
+        </SignInButton>
+      </SignedOut>
+
+      {/* TODO: Delete this when we have a proper log out. */}
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
     </div>
    );
 }
