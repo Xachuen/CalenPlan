51feeb53398089fb56a9a1e34b48c1b20ef5a2d9
Xavier <xachuen@gmail.com>
2024-08-19 16:36:27 -0700

    Add friend ui

--- front_end/components/NavBar/subcomponents/FriendsButton/FriendsButton.jsx
+++ front_end/components/NavBar/subcomponents/FriendsButton/FriendsButton.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Dropdown } from 'react-bootstrap';
 import styles from './FriendsButton.module.css';
 
 const FriendsButton = ( { className } ) => {
-  return ( 
-    <img className={`${className} ${styles.FriendsButton}`} src='front_end\src\assets\General\group.svg'/>
+  // These handle showing the drop down.
+  const [show, setShow] = useState(false);
+  const toggleDropdown = () => setShow(!show);
+
+  return (
+    <>
+    <div className={styles.DropDownContainer}>
+      <img 
+      className={`${className} ${styles.FriendsButton}`} 
+      src='front_end\src\assets\General\group.svg'
+      onClick={toggleDropdown} 
+      />
+      <Dropdown show={show} onToggle={toggleDropdown} align="end">
+        <Dropdown.Menu className={`${styles.DropDownPosition} ${styles.CustomMenu}`}>
+          <div className={styles.FriendsList}>
+            <Dropdown.Item href="#/action-1">Friend</Dropdown.Item>
+            <Dropdown.Item href="#/action-1">Friend</Dropdown.Item>
+            <Dropdown.Item href="#/action-1">Friend</Dropdown.Item>
+            <Dropdown.Item href="#/action-1">Friend</Dropdown.Item>
+            <Dropdown.Item href="#/action-1">Friend</Dropdown.Item>
+            <Dropdown.Item href="#/action-1">Friend</Dropdown.Item>
+            <Dropdown.Item href="#/action-1">Friend</Dropdown.Item>
+            <Dropdown.Item href="#/action-1">Friend</Dropdown.Item>
+            <Dropdown.Item href="#/action-1">Friend</Dropdown.Item>
+          </div>
+          <Dropdown.Divider />
+          <form>
+            <input className={styles.FriendEmailInput}
+            placeholder="Friend's Email"></input>
+            <button className={styles.AddFriendButton}>Add</button>
+          </form>
+        </Dropdown.Menu>
+      </Dropdown> 
+    </div>
+    </>
    );
 }
  
